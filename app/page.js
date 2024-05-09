c13eeab266729915ec33c9aad1ecbd78494ebf1a
Leonardo <leonardo.monsalve@empirekeeway.com>
2024-05-09 11:09:23 -0400

    Changing the px padding in the page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-0">
+    <main className="flex min-h-screen flex-col items-center justify-between py-0 px-24">
       <ZeroHeightTabsV2 />
     </main>
   );
